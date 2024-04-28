e78d8a1ba0971cb2c873538e6ea141efc217cff1
Ganesh-Tammana <hareeramaa321@gmail.com>
2024-04-28 16:46:24 +0530

    commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import bodyParser from 'body-parser';
 import usersRouter from './routes/users.js';
 
 const app=express();
-const PORT=3000
+const PORT=3000;
 
 app.use(bodyParser.json());
 app.use('/users',usersRouter);
 
 app.get('/',(req,res)=>{
-    res.send('mre')
+    res.send('mre');
 })
 
 app.listen(PORT,()=> console.log(`hell0 ${PORT}`))
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,44 @@
 import express from 'express';
 
+import { v4 as uuidv4 } from 'uuid';
+
+
 const router = express.Router();
 
-const users=[
-    {
-        firstName:"john",
-        lastName:"marcus",
-        age:23
-    },
-    {
-        firstName:"jonyy",
-        lastName:"peter",
-        age:23
-    }
-]
+let users=[];
 
+//Getting the data from the database
 router.get('/',(req,res)=>{
-    
     res.send(users);
 })
 
+//Sending the data to the database
 router.post('/',(req,res)=>{
     const user=req.body
-    users.push(user)
-    res.send(`User added sussessfully ${user.firstName}`);
+
+    //uuidv4 is used for providing unique id to the user
+    users.push({...user,Id:uuidv4()});
+
+    res.send(`User added sussessfully ${user.firstName} added to database`);
 })
 
-export default router
+//Getting the data of specific user 
+router.get('/:id',(req,res)=>{
+    const {id}=req.params;
+
+    const foundUser=users.find((user)=> user.Id===id);
+    res.send(foundUser);
+})
+
+//Delete the user from the database
+router.delete('/:id',(req,res)=>{
+    const {id}=req.params;
+
+    users=users.filter((user)=>
+       
+        console.log(user.Id!==id)
+    );
+
+    res.send('user deleted successfully');
+})
+export default router;
